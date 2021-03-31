1e2cf140e8596196c194d8afe828922536fbdabb
nikhil <nikhiltomy@au-ki.com>
2021-03-31 23:05:33 +0530

    empty state example

--- my_test_app/pages/index.js
+++ my_test_app/pages/index.js
@@ -1,34 +1,37 @@
-import { Heading, Page } from "@shopify/polaris";
+import { EmptyState, Layout, Page} from "@shopify/polaris";
 import { ResourcePicker } from "@shopify/app-bridge-react";
 import { Component } from "react";
 
 class Index extends Component {
- state={open:false}
+  state={open:false}
   render() {
     return (
-      <Page
-        fullWidth
-        title="Product Selector"
-        primaryAction={{
-          content: 'Select Products',
-          onAction: () => this.setState({open:true})
-        }}
-      >
-       <ResourcePicker
-        resourceType='Product'
-        open={this.state.open}
-        onCancel={()=>this.setState({open:false})}
-        onSelection={(resources)=>this.handleselection(resources)}
-       />
+      <Page>
+        <Layout>
+          <EmptyState
+            heading="Manage your inventory transfers"
+            action={{ content: 'Select Products' }}
+            secondaryAction={{ content: 'Learn more', url: 'https://help.shopify.com' }}
+            image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+          >
+            <p>Track and receive your incoming inventory from suppliers.</p>
+          </EmptyState>
+        </Layout>
+        <ResourcePicker
+          resourceType='Product'
+          open={this.state.open}
+          onCancel={() => this.setState({ open: false })}
+          onSelection={(resources) => this.handleselection(resources)}
+        />
       </Page>
     )
+    
   }
-  handleselection=(resources)=>{
-    const idFromResources= resources.selection.map((product)=> product.id)
-    this.setState({open:false})
+  handleselection = (resources) => {
+    const idFromResources = resources.selection.map((product) => product.id)
+    this.setState({ open: false })
     console.log(idFromResources);
   }
-
 }
 
 export default Index;
